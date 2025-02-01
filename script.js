2fb082d357cfd7288c75b3a064e58925137f9f2c
cazemirocode <bannxmobile@gmail.com>
2025-02-01 18:44:48 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 function revealProfile() {
-    document.getElementById('profile-card').style.display = 'block';
-    document.getElementsByClassName('social-icons')[0].style.display = 'flex';
+    document.getElementById('profile-content').style.display = 'flex';
     document.getElementById('reveal-button').style.display = 'none';
     document.getElementById('background-music').play();
 }
